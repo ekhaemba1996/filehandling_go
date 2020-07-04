eb0a2afd375e4c86865b0b1d858283651796c044
Elton Mwale <ekhaemba1996@gmail.com>
2020-07-04 13:26:57 -0400

    ParseFasta function

--- fileparser/fileparser.go
+++ fileparser/fileparser.go
@@ -2,6 +2,7 @@ package fileparser
 import (
     "fmt"
     "io/ioutil"
+    "regexp"
 )
 
 func ParseFile(path string) string{
@@ -11,4 +12,19 @@ func ParseFile(path string) string{
         return ""
 	}
 	return string(data)
+}
+
+func ParseFasta(fasta string) map[string]string{
+	fastaRegexString := `>(?P<key>Rosalind_\d+)\r?\n(?P<entire_sequence>(?P<seq_line>[GATC]+\r?\n?)+)`
+	reReplaceNewline := regexp.MustCompile(`(\r?\n)`)
+	fastaRegex := regexp.MustCompile(fastaRegexString)
+	resultMap := make(map[string]string)
+
+	for _, submatches := range fastaRegex.FindAllStringSubmatch(fasta, -1){
+		key := submatches[1]
+		value := submatches[2]
+		value = reReplaceNewline.ReplaceAllString(value, "")
+		resultMap[key] = value
+	}
+	return resultMap
 }
